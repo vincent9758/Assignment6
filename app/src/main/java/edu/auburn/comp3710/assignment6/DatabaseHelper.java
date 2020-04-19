986ffe889c04619ad68cf5a75afa84456ebbd538
wenshen <vincent199758@gmail.com>
2020-04-18 22:51:56 -0500

    Initial commit

---
+++ app/src/main/java/edu/auburn/comp3710/assignment6/DatabaseHelper.java
@@ -0,0 +1,73 @@
+package edu.auburn.comp3710.assignment6;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "SpendingManagement.db";
+    public static final String TABLE_NAME = "Money";
+
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, 4);
+    }
+
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int NewVersion){
+        onCreate(db);
+    }
+
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL("CREATE TABLE " + TABLE_NAME + " (id INTEGER PRIMARY KEY AUTOINCREMENT, Date int, Amount DOUBLE, Event varchar(255))");
+    }
+
+
+    public void Create(int date, double amount, String event){
+        SQLiteDatabase database = this.getReadableDatabase();
+        ContentValues content = new ContentValues();
+        content.put("Date", date);
+        content.put("Amount", amount);
+        content.put("Event", event);
+        database.insert(TABLE_NAME,null, content);
+    }
+
+    public Cursor SearchData(String Sdate,String Edate,String LowBoundPrice,String HighBoundPrice) {
+
+
+
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor result = null;
+
+        if (LowBoundPrice.equals("n") && HighBoundPrice.equals("n")){
+            int getSdate = Integer.parseInt(Sdate);
+            int getEdate = Integer.parseInt(Edate);
+            result = db.rawQuery("SELECT * FROM"+ TABLE_NAME+ " WHERE Date >= " +getSdate + " AND Date <= " + getEdate,null);
+        }
+
+        else if (Sdate.equals("n") && Edate.equals("n")){
+            double getLowBound = Double.parseDouble(LowBoundPrice);
+            double getHighBound = Double.parseDouble(HighBoundPrice);
+            result = db.rawQuery("SELECT * FROM"+ TABLE_NAME+ " WHERE Amount >= " + getLowBound + " AND Amount <= " + getHighBound,null);
+        }
+
+        else{
+            int getSdate = Integer.parseInt(Sdate);
+            int getEdate = Integer.parseInt(Edate);
+            double getLowBound = Double.parseDouble(LowBoundPrice);
+            double getHighBound = Double.parseDouble(HighBoundPrice);
+            result = db.rawQuery("SELECT * FROM"+ TABLE_NAME+ " WHERE Amount >= " + getLowBound + " AND Amount <= " + getHighBound + " AND Date >=" + getSdate + " AND Date <=" + getEdate,null);
+        }
+
+
+        return result;
+    }
+
+
+    public Cursor ReadData(){
+        SQLiteDatabase database = this.getReadableDatabase();
+        Cursor result = database.rawQuery("SELECT * FROM " + TABLE_NAME, null);
+        return result;
+    }
+}
---
+++ app/src/main/java/edu/auburn/comp3710/assignment6/MainActivity.java
@@ -0,0 +1,216 @@
+package edu.auburn.comp3710.assignment6;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+import android.widget.TableRow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    DatabaseHelper Database;
+    TextView balance;
+    EditText day,month,year;
+    EditText amount;
+    EditText event;
+    Button btnAdd;
+    Button btnSub;
+    TableLayout history;
+    EditText Sdate,Edate;
+    EditText LowBoundPrice, HighBoundPrice;
+    Button search;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Database = new DatabaseHelper(this);
+
+        balance = findViewById(R.id.balance);
+        day =  findViewById(R.id.day);
+        month =findViewById(R.id.month);
+        year = findViewById(R.id.year);
+        amount = findViewById(R.id.editAmount);
+        event = findViewById(R.id.editEvent);
+        btnAdd =  findViewById(R.id.btnAdd);
+        btnSub = findViewById(R.id.btnSub);
+        history =  findViewById(R.id.table);
+        Sdate = findViewById(R.id.Sdate);
+        Edate = findViewById(R.id.Edate);
+        LowBoundPrice = findViewById(R.id.Sprice);
+        HighBoundPrice = findViewById(R.id.Eprice);
+        search = findViewById(R.id.search);
+        AddEvent();
+        GetHistory();
+    }
+
+    public void AddEvent(){
+        btnAdd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        double Amount = Double.parseDouble(amount.getText().toString());
+                        int date = Integer.parseInt(month.getText().toString() + day.getText().toString() + year.getText().toString());
+                        Database.Create(date, Amount, event.getText().toString());
+                        GetHistory();
+                        reset();
+                    }
+                }
+        );
+
+        btnSub.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        double Amount =  -1* Double.parseDouble(amount.getText().toString());
+                        int date = Integer.parseInt(month.getText().toString() + day.getText().toString() + year.getText().toString());
+                        Database.Create(date, Amount, event.getText().toString());
+                        GetHistory();
+                        reset();
+                    }
+                }
+        );
+
+        search.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String getSdate,getEdate,getLowBound,getHighBound;
+                        getSdate = Sdate.getText().toString();
+                        getEdate = Edate.getText().toString();
+                        getLowBound = LowBoundPrice.getText().toString();
+                        getHighBound = HighBoundPrice.getText().toString();
+                        if (getLowBound.equals("") && getLowBound.equals("") && getSdate.length() > 0 && getEdate.length() > 0) {
+                            search(getSdate,getEdate,"n","n");
+                        }
+                        else if (getSdate.equals("") && getEdate.equals("") && getLowBound.length() > 0 && getHighBound.length() > 0) {
+                            search("n","n",getLowBound,getHighBound);
+                        }
+                        else if (getSdate.equals("") && getEdate.equals("") && getLowBound.length() >0 && getHighBound.equals("")) {
+                            getHighBound = "1000000";
+                            search("n","n",getLowBound,getHighBound);
+                        }
+                        else if (getLowBound.equals("") && getHighBound.equals("") && getEdate.equals("") && getSdate.length() > 0) {
+                            getHighBound = "1000000";
+                            search(getSdate,getEdate,"n","n");
+                        }
+
+                        else if (getEdate.length() > 0 && getSdate.length() > 0 && getLowBound.length() > 0 && getHighBound.length() > 0) {
+                            search(getSdate,getEdate,getLowBound,getHighBound);
+                        }
+
+                        else if (getEdate.equals("") && getSdate.length() > 0 && getHighBound.length() > 0 && getLowBound.length() > 0) {
+                            getHighBound = "1000000";
+                            search(getSdate,getEdate,getLowBound,getHighBound);
+                        }
+
+                        else if (getEdate.length() > 0 && getSdate.length() > 0 && getHighBound.equals("") && getLowBound.length() > 0) {
+                            getHighBound = "99999999";
+                            search(getSdate,getEdate,getLowBound,getHighBound);
+                        }
+                        reset();
+
+                    }
+                }
+        );
+
+    }
+
+    public void GetHistory(){
+        Cursor result = Database.ReadData();
+        Double balance = 0.0;
+
+        if (history.getChildCount() > 0) {
+            history.removeAllViews();
+
+        }
+
+        while(result.moveToNext()){
+            double price = Double.parseDouble(result.getString(2));
+            balance += price;
+            TableRow t = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams();
+            lp.weight = 1;
+
+
+            TextView Date = new TextView(this);
+            Date.setLayoutParams(lp);
+            String getDate = result.getString(1);
+            String sTime = getDate.substring(0,2) + "/" + getDate.substring(2,4) + "/" + getDate.substring(4,8);
+            Date.setText(sTime);
+            t.addView(Date);
+
+            TextView amount = new TextView(this);
+            amount.setLayoutParams(lp);
+            amount.setText(result.getString(2));
+            t.addView(amount);
+
+            TextView categroy = new TextView(this);
+            categroy.setLayoutParams(lp);
+            categroy.setText(result.getString(3));
+            t.addView(categroy);
+
+            history.addView(t, new TableLayout.LayoutParams());
+
+        }
+        MainActivity.this.balance.setText("Current Balance: $" + Double.toString(balance));
+    }
+
+    public void search(String Sdate,String Edate,String LowBoundPrice,String HighBoundPrice){
+        Double balance = 0.0;
+        Cursor data = Database.SearchData(Sdate, Edate, LowBoundPrice, HighBoundPrice);
+
+        if (history.getChildCount() > 0) {
+            history.removeAllViews();
+
+        }
+
+        while(data.moveToNext()){
+            double price = Double.parseDouble(data.getString(2));
+            balance += price;
+            TableRow t = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams();
+            lp.weight = 1;
+
+            TextView Date = new TextView(this);
+            Date.setLayoutParams(lp);
+            String getDate = data.getString(1);
+            String sTime = getDate.substring(0,2) + "/" + getDate.substring(2,4) + "/" + getDate.substring(4,8);
+            Date.setText(sTime);
+            t.addView(Date);
+
+            TextView amount = new TextView(this);
+            amount.setLayoutParams(lp);
+            amount.setText(data.getString(2));
+            t.addView(amount);
+
+            TextView categroy = new TextView(this);
+            categroy.setLayoutParams(lp);
+            categroy.setText(data.getString(3));
+            t.addView(categroy);
+
+            history.addView(t, new TableLayout.LayoutParams());
+
+        }
+        MainActivity.this.balance.setText("Current Balance: $" + Double.toString(balance));
+    }
+
+    public void reset(){
+        MainActivity.this.amount.setText("");
+        MainActivity.this.event.setText("");
+        MainActivity.this.Sdate.setText("");
+        MainActivity.this.Edate.setText("");
+        MainActivity.this.LowBoundPrice.setText("");
+        MainActivity.this.HighBoundPrice.setText("");
+        MainActivity.this.day.setText("");
+        MainActivity.this.month.setText("");
+        MainActivity.this.year.setText("");
+
+    }
+}
